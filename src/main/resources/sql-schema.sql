7101110704296eb9a94177fd5618e97ca27dfa7d
Matthew Cameron <matthew91c@gmail.com>
2020-11-17 10:28:38 +0000

    schema updated, removed the foreign key reference to orders table

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,13 +1,13 @@
+
 drop schema ims;
 CREATE SCHEMA IF NOT EXISTS `ims`;
 USE `ims` ;
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `firstName` VARCHAR(40) NULL DEFAULT NULL,
+    `first_name` VARCHAR(40) NULL DEFAULT NULL,
     `surname` VARCHAR(40) NULL DEFAULT NULL,
     `order_id_fk` INT(11) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`order_id_fk`) REFERENCES orders(`order_id`)
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`items` (
@@ -32,12 +32,4 @@ CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
 );
 
 
-drop schema ims;
-CREATE SCHEMA IF NOT EXISTS `ims`;
-USE `ims` ;
-CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) NULL DEFAULT NULL,
-    `surname` VARCHAR(40) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`)
-);
+
