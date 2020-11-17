dcfa2fd51c3ad98e57ec21e398e8816b12184e9c
Matthew Cameron <matthew91c@gmail.com>
2020-11-17 10:12:21 +0000

    Updated the schema to include items, orders and order_items

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -3,6 +3,40 @@ CREATE SCHEMA IF NOT EXISTS `ims`;
 USE `ims` ;
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(40) NULL DEFAULT NULL,
+    `surname` VARCHAR(40) NULL DEFAULT NULL,
+    `order_id_fk` INT(11) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`order_id_fk`) REFERENCES orders(`order_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+	`item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `itemName` VARCHAR(40) NULL DEFAULT NULL,
+    `itemValue` DOUBLE(200,2) NULL DEFAULT NULL,
+    PRIMARY KEY (`item_id`)
+);
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+	`order_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id_fk` INT(11) NULL DEFAULT NULL,
+    `orderItems_id_fk` INT(11) NULL DEFAULT NULL,
+    PRIMARY KEY (`order_id`),
+    FOREIGN KEY (`customer_id_fk`) REFERENCES customers(`id`),
+    FOREIGN KEY (`orderItems_id_fk`) REFERENCES order_items(`orderItems_id`)
+);
+CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
+	`order_items_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `order_id_fk` INT(11) NULL DEFAULT NULL,
+    PRIMARY KEY (`order_items_id`),
+    FOREIGN KEY (`order_id_fk`) REFERENCES orders(`order_id`)
+);
+
+
+drop schema ims;
+CREATE SCHEMA IF NOT EXISTS `ims`;
+USE `ims` ;
+CREATE TABLE IF NOT EXISTS `ims`.`customers` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) NULL DEFAULT NULL,
     `surname` VARCHAR(40) NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
